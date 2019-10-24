69d6a43739db7df8c0ccb69479b698109f5d0e7e
amed12 <amedixro@gmail.com>
2019-10-24 19:10:17 +0700

    SQlite part 1

---
+++ app/src/main/java/com/achmad/madeacademy/dicodingmadeclass/NoteAdapter.kt
@@ -0,0 +1,69 @@
+package com.achmad.madeacademy.dicodingmadeclass
+
+import android.app.Activity
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.achmad.madeacademy.dicodingmadeclass.entity.Note
+import kotlinx.android.synthetic.main.item_note.view.*
+
+class NoteAdapter(private val activity: Activity) : RecyclerView.Adapter<NoteAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteAdapter.ViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_note,parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun getItemCount(): Int = this.listNotes.size
+
+
+    override fun onBindViewHolder(holder: NoteAdapter.ViewHolder, position: Int) {
+        holder.bind(listNotes[position])
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        fun bind(note: Note){
+            with(itemView){
+                tv_item_title.text = note.title
+                tv_item_date.text = note.date
+                tv_item_description.text = note.description
+                cv_item_note.setOnClickListener(CustomOnItemClickListener(adapterPosition,object : CustomOnItemClickListener.OnItemClickCallback{
+                    override fun onItemClicked(view: View?, position: Int) {
+                        val intent = Intent(activity, NoteAddUpdateActivity::class.java)
+                        intent.putExtra(NoteAddUpdateActivity.EXTRA_POSITION, position)
+                        intent.putExtra(NoteAddUpdateActivity.EXTRA_NOTE, note)
+                        activity.startActivityForResult(intent, NoteAddUpdateActivity.REQUEST_UPDATE)
+                    }
+                }))
+            }
+        }
+    }
+
+    var listNotes = ArrayList<Note>()
+        set(listNotes){
+            if (listNotes.size > 0){
+                this.listNotes.clear()
+            }
+            this.listNotes.addAll(listNotes)
+            notifyDataSetChanged()
+        }
+
+    fun addItem(note: Note){
+        this.listNotes.add(note)
+        notifyItemInserted(this.listNotes.size - 1)
+    }
+
+    fun updateItem(position: Int, note: Note){
+        this.listNotes[position] = note
+        notifyItemChanged(position,note)
+    }
+
+    fun removeItem(position: Int){
+        this.listNotes.removeAt(position)
+        notifyItemRemoved(position)
+        notifyItemRangeChanged(position, this.listNotes.size)
+    }
+}
---
+++ app/src/main/java/com/achmad/madeacademy/dicodingmadeclass/helper/MappingHelper.kt
@@ -0,0 +1,21 @@
+package com.achmad.madeacademy.dicodingmadeclass.helper
+
+import android.database.Cursor
+import com.achmad.madeacademy.dicodingmadeclass.entity.DatabaseContract
+import com.achmad.madeacademy.dicodingmadeclass.entity.Note
+
+object MappingHelper{
+    fun mapCursorToArrayList(noteCursor: Cursor): ArrayList<Note>{
+        val noteList = ArrayList<Note>()
+        noteCursor.moveToFirst()
+        while (noteCursor.moveToNext()){
+            val id = noteCursor.getInt(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+            noteList.add(Note(id, title, description, date))
+        }
+
+        return noteList
+    }
+}
