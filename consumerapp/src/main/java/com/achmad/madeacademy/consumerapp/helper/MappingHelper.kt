b0a326a77ce2b60e7fb9c750dff2182999e3563c
amed12 <amedixro@gmail.com>
2019-10-31 16:47:00 +0700

    Create consumer app to get data from mynotesapp using content provider

---
+++ consumerapp/src/main/java/com/achmad/madeacademy/consumerapp/helper/MappingHelper.kt
@@ -0,0 +1,30 @@
+package com.achmad.madeacademy.consumerapp.helper
+
+import android.database.Cursor
+import com.achmad.madeacademy.consumerapp.entity.DatabaseContract
+import com.achmad.madeacademy.consumerapp.entity.Note
+
+object MappingHelper{
+    fun mapCursorToArrayList(noteCursor: Cursor): ArrayList<Note>{
+        val noteList = ArrayList<Note>()
+        noteCursor.moveToFirst()
+        while (noteCursor.moveToNext()){
+            val id = noteCursor.getInt(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+            val title = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+            val description = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+            val date = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+            noteList.add(Note(id, title, description, date))
+        }
+
+        return noteList
+    }
+
+    fun mapCursorToObject(noteCursor: Cursor): Note {
+        noteCursor.moveToNext()
+        val id = noteCursor.getInt(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns._ID))
+        val title = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.TITLE))
+        val description = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DESCRIPTION))
+        val date = noteCursor.getString(noteCursor.getColumnIndexOrThrow(DatabaseContract.NoteColumns.DATE))
+        return Note(id, title, description, date)
+    }
+}
