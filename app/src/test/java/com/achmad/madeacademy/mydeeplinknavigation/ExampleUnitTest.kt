1cd49ed1ea2e226be9763b812391d0596ad4cc85
amed12 <amedixro@gmail.com>
2019-11-19 20:28:06 +0700

    finish task

--- app/src/test/java/com/achmad/madeacademy/mydeeplinknavigation/ExampleUnitTest.kt
+++ app/src/test/java/com/achmad/madeacademy/mydeeplinknavigation/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
-package com.achmad.madeacademy.dicodingmadeclass
+package com.achmad.madeacademy.mydeeplinknavigation
 
+import org.junit.Assert.assertEquals
 import org.junit.Test
-
-import org.junit.Assert.*
 
 /**
  * Example local unit test, which will execute on the development machine (host).
